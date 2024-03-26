b4d6de744b29b08055707e94c7e82c0e1750de15
Glauton Osório <glautonleao@gmail.com>
2024-03-26 05:04:33 -0300

    fix: Arrumando erro em relação a perda de arquivos

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,23 +1,25 @@
 //Mapeando HTML
-const botaoIniciar = document.getElementById("botao-iniciar")
-const containerTimerFoco = document.getElementById("containerTimerFoco")
-const botaoStop = document.getElementById("botao-stop")
-const containerTimerDescanso = document.getElementById("containerTimerDescanso")
-const botaoConcluido = document.getElementById("botao-concluido")
-const circulo = document.getElementById("circulo")
-const body = document.getElementById("body")
-const botaoPausar = document.getElementById("botao-pausar")
-const botaoRetomar = document.getElementById("botao-retomar")
-const timerDisplay = document.getElementById("timerDisplay")
-
-
+const botaoIniciar = document.getElementById("botao-iniciar");
+const containerTimerFoco = document.getElementById("containerTimerFoco");
+const botaoStop = document.getElementById("botao-stop");
+const containerTimerDescanso = document.getElementById(
+  "containerTimerDescanso"
+);
+const botaoConcluido = document.getElementById("botao-concluido");
+const circulo = document.getElementById("circulo");
+const body = document.getElementById("body");
+const botaoPausar = document.getElementsByClassName("botao-pausar");
+const botaoRetomar = document.getElementsByClassName("botao-retomar");
+const timerDisplay = document.getElementById("timerDisplay");
+const botaoAvancar = document.getElementsByClassName("botao-avancar");
+const containerAlongamentoConcluido = document.getElementById(
+  "container-alongamento-concluido"
+);
+const botaoAlongarSe = document.getElementById("botao-alongar");
 
 let timer; // Variável para armazenar o identificador do setInterval
 let timerRunning = false; // Variável para controlar se o timer está em execução
 let milliseconds = 0; // Variável para armazenar os milissegundos
-
-
-
 
 function startTimer() {
   if (!timerRunning) {
@@ -25,20 +27,56 @@ function startTimer() {
     timer = setInterval(updateTimer, 10); // Inicia o intervalo de atualização do timer a cada 10 milissegundos
     botaoIniciar.style.display = "none";
     containerTimerFoco.style.display = "flex";
-    botaoRetomar.style.display = "none";
+    botaoRetomar[0].style.display = "none";
     botaoStop.style.display = "none";
-    botaoPausar.style.display = "block";
+    botaoPausar[0].style.display = "block";
     circulo.style.animation = "grow 10s forwards";
   }
 }
 
-function pauseTimer() {
+function pauseTimer(local) {
   clearInterval(timer); // Para o intervalo do timer
   timerRunning = false; // Desativa o timer
-  circulo.style.animationPlayState = 'paused';
-  botaoPausar.style.display = 'none';
-  botaoRetomar.style.display = 'block';
-  botaoStop.style.display = 'block';
+  circulo.style.animationPlayState = "paused";
+
+  const div = local;
+  if (div.id === "containerTimerFoco") {
+    botaoStop.style.display = "block";
+    botaoRetomar[0].style.display = "block";
+    botaoAvancar[0].style.display = "none";
+    botaoPausar[0].style.display = "none";
+  }
+  if (div.id === "containerTimerDescanso") {
+    botaoPausar[1].style.display = "none";
+    botaoAvancar[1].style.display = "block";
+    botaoAlongarSe.style.display = "none";
+    botaoRetomar[1].style.display = "block";
+  }
+}
+
+function avancar(local) {
+  const div = local;
+  if (div.id === "containerTimerFoco") {
+    clearInterval(timer); // Para o intervalo do timer
+    timerRunning = false; // Desativa o timer
+    milliseconds = 0; // Reseta os milissegundos
+    TelaDescanso();
+  }
+
+  if (div.id === "containerTimerDescanso") {
+    clearInterval(timer); // Para o intervalo do timer
+    timerRunning = false; // Desativa o timer
+    milliseconds = 0; // Reseta os milissegundos
+    containerTimerDescanso.style.display = "none";
+    botaoIniciar.style.display = "block";
+    circulo.style.animation = "none";
+    body.style.backgroundImage = "url(assets/foco.png)";
+    botaoRetomar[1].style.display = "none";
+    botaoAvancar[1].style.display = "none";
+    containerAlongamentoConcluido.style.display = "none";
+    botaoPausar[1].style.display = "block";
+    botaoAlongarSe.style.display = "block";
+  }
 }
 
 function interromperTimer() {
@@ -50,13 +88,27 @@ function interromperTimer() {
   circulo.style.animation = "none";
 }
 
-function retomarTimer() {
+function retomarTimer(local) {
   timerRunning = true; // Ativa o timer
   timer = setInterval(updateTimer, 10); // Inicia o intervalo de atualização do timer a cada 10 milissegundos
-  circulo.style.animationPlayState = 'running';
-  botaoPausar.style.display = 'block';
-  botaoRetomar.style.display = 'none';
-  botaoStop.style.display = 'none';
+  circulo.style.animationPlayState = "running";
+
+  const div = local;
+  if (div.id === "containerTimerFoco") {
+    botaoPausar[0].style.display = "block";
+    botaoRetomar[0].style.display = "none";
+    botaoStop.style.display = "none";
+    botaoAvancar[0].style.display = "block";
+  }
+  if (div.id === "containerTimerDescanso") {
+    botaoPausar[1].style.display = "block";
+    botaoAvancar[1].style.display = "none";
+    botaoRetomar[1].style.display = "none";
+
+    if (containerAlongamentoConcluido.style.display !== "flex") {
+      botaoAlongarSe.style.display = "block";
+    }
+  }
 }
 
 function updateTimer() {
@@ -65,17 +117,36 @@ function updateTimer() {
   const minutes = Math.floor((milliseconds / 6000) % 60); // Calcula os minutos
   const seconds = Math.floor((milliseconds / 100) % 60); // Calcula os segundos
   const milisecs = milliseconds % 100; // Calcula os milissegundos
-  displayElement.textContent = `${(minutes < 10 ? "0" : "") + minutes}:${(seconds < 10 ? "0" : "") + seconds
-    }.${(milisecs < 10 ? "00" : milisecs < 100 ? "0" : "") + milisecs}`; // Atualiza o display com o tempo formatado
+  displayElement.textContent = `${(minutes < 10 ? "0" : "") + minutes}:${
+    (seconds < 10 ? "0" : "") + seconds
+  }`; // Atualiza o display com o tempo formatado
+  // .${(milisecs < 10 ? "00" : milisecs < 100 ? "0" : "") + milisecs} Supressão da apresentação do milissegundos
 
   if (minutes === 25 && seconds === 0 && milisecs === 0) {
-    body.style.backgroundImage = "url(assets/descanso.png)";
-    containerTimerFoco.style.display = "none";
-    circulo.style.animation = "none";
-    circulo.style.animation = "pulse 12s 25";
-    containerTimerDescanso.style.display = "flex";
-    alert(
-      "Pomodoro completo! Agora aperte o botão para saber o exercício que fará por 5 minutos."
-    ); // Exibe uma mensagem informando que o Pomodoro foi completado
+    TelaDescanso();
+
+    // alert(
+    //   "Pomodoro completo! Agora aperte o botão para saber o exercício que fará por 5 minutos."
+    // ); // Exibe uma mensagem informando que o Pomodoro foi completado
   }
+}
+
+//Apresenta a tela de descanso
+function TelaDescanso() {
+  body.style.backgroundImage = "url(assets/descanso.png)";
+  containerTimerFoco.style.display = "none";
+  circulo.style.animation = "none";
+  circulo.style.animation = "pulse 12s 25";
+  containerTimerDescanso.style.display = "flex";
+}
+
+function alongarSe() {
+  //chama a função que altera o html -----> GetExercise
+  botaoAlongarSe.style.display = "none";
+  containerAlongamentoConcluido.style.display = "flex";
+}
+
+function concluir() {
+  containerAlongamentoConcluido.style.display = "none";
+  botaoAlongarSe.style.display = "block";
 }
